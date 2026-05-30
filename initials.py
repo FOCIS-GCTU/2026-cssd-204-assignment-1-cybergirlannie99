89f23fcd226f6691695ddf77248d76dc3a4de54a
Agyepoma Nyarko <134434999+cybergirlannie99@users.noreply.github.com>
2026-05-30 15:53:51 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials in bold letters
+# Assignment Number: 1
+#
+# Name: Nyarko Ama Agyepoma
+# STUDENT ID:  2425403261
+# Email: agyepomanyarko@gmail.com
+# Grader: Augustus Buckman
+#
+# On my honor, Nyarko Ama Agyepoma, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+ #Function to print my initials in bold letters
 
+    print("")
+    print("...NAA")
+    print("")
+
+    print("...NNN.......NN.............AAA.................AAA.........")
+    print("...NNNN......NN...........AAA.AAA.............AAA.AAA.......")
+    print("...NN.NN.....NN..........AAA...AAA...........AAA...AAA......")
+    print("...NN..NN....NN.........AAA.....AAA.........AAA.....AAA.....")
+    print("...NN...NN...NN........AAA......AAA........AAA......AAA.....")
+    print("...NN....NN..NN........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print("...NN.....NN.NN........AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print("...NN......NNNN........AAA......AAA........AAA......AAA.....")
+    print("...NN.......NNN...**...AAA......AAA...**...AAA......AAA...**")
+    print("...NN........NN...**...AAA......AAA...**...AAA......AAA...**")
+
+    print("")
 
 
 main()
